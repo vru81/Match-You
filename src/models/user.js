acce2bd53482c8bc1eaae39af710ff9dbf2206f5
vrutik <vrutiksurti81@gmail.com>
2026-08-23 18:21:35 +0530

    exploring validtor library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address");
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong enough");
+            }
+        }
     },
     age: {
         type: Number,
@@ -36,6 +47,11 @@ const userSchema = new mongoose.Schema({
     photoUrl:{
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL");
+            }
+        }
     },
     about: {
         type:String,
